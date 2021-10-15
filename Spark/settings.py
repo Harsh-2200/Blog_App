3bdf9d6cabf173ba648893e74477735e1249d461
Harsh-2200 <harshharodenice1@gmail.com>
2021-10-15 10:40:59 +0530

    idk

--- Spark/settings.py
+++ Spark/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j---svx4-b3*hwi4@7#gxh%blgfhvs4d!=@74s1$1p445(j391'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
